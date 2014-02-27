778b2e8f78fc9bbaa3fb4525a86063d76f9b9f1b
Daniel Keller <dbk40@case.edu>
2014-02-27 00:08:09 -0500

    added netwire-vinyl adapter

---
+++ src/Util.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE DataKinds, TypeOperators, Arrows, GADTs, TypeFamilies,
+    MultiParamTypeClasses, FlexibleInstances, FlexibleContexts #-}
+module Util (
+    bundle
+) where
+
+import Prelude hiding ((.))
+import Data.Vinyl as VR
+import Control.Wire
+
+type family Bundled rw :: [*]
+type instance Bundled (PlainRec '[]) = '[]
+type instance Bundled (PlainRec (sy ::: (Wire s e m a b) ': ws)) = sy ::: b ': Bundled (PlainRec ws)
+
+class Bundle rw w where
+    bundle :: rw -> w (PlainRec (Bundled rw))
+
+instance Monad m => Bundle (PlainRec '[]) (Wire s e m a) where
+    bundle RNil = pure RNil
+
+instance (Monad m, Bundle (PlainRec rws) (Wire s e m a)) =>
+        Bundle (PlainRec (sy ::: (Wire s e m a b) ': rws)) (Wire s e m a) where
+    bundle (Identity r :& rs) = proc inp -> do
+        r' <-r-< inp
+        rs' <-bundle rs-< inp
+        returnA -< (Identity r' :& rs')
